8583fe91cce5af0c176b4c7c8d0cb8f49631d4a7
Fabian Gonzalez <fabiandres9106@gmail.com>
2024-11-06 19:55:06 -0500

    Se agregó el router Roles en roiter de api

--- ticket_app_backend/app/api/router.py
+++ ticket_app_backend/app/api/router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from app.api.endpoints import user
+from app.api.endpoints import role
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/users", tags=["users"])
+api_router.include_router(role.router, prefix="/roles", tags=["roles"])
