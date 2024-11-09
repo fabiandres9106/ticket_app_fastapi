d82a6d0393b71e336c3a32728977f5f72ee6b227
Fabian Gonzalez <fabiandres9106@gmail.com>
2024-11-08 23:38:09 -0500

    Se agregó la autenticación de usuarios

--- ticket_app_backend/main.py
+++ ticket_app_backend/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.router import api_router
 
 app = FastAPI()
+
+# Configuración de CORS
+origins = [
+    "http://localhost:3000",  # Origen del frontend de React en desarrollo
+    "http://127.0.0.1:3000",  # Alternativa para localhost
+    # Agrega otros orígenes según sea necesario, como tu dominio en producción
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
